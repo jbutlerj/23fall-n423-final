d113a7f9c6c417d49e463ac38a88ae58373d9265
jbutlerj <144062324+jbutlerj@users.noreply.github.com>
2023-12-07 21:11:09 -0500

    created various blank pages for layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import React from "react";
+import "semantic-ui-css/semantic.css";
+import Link from "next/link";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    );
 }
